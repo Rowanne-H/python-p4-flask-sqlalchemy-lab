290162ce7de242bdfcc04378bd1726dd8b67faa8
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-25 23:28:28 +1000

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,23 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import MetaData 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(naming_convention={ 
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s", 
+    }) 
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy(metadata=metadata) 
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-class Animal(db.Model):
-    __tablename__ = 'animals'
-
-    id = db.Column(db.Integer, primary_key=True)
+class Zookeeper(db.Model): 
+    __tablename__ = 'zookeepers' 
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String) 
+    birthday = db.Column(db.Date, nullable=True) 
+    animals = db.relationship('Animal', backref='zookeeper') 
+    
+class Enclosure(db.Model): 
+    __tablename__ = 'enclosures' 
+    id = db.Column(db.Integer, primary_key=True) 
+    environment = db.Column(db.String) 
+    open_to_visitors = db.Column(db.Boolean, default=False, nullable=False) 
+    animals = db.relationship('Animal', backref='enclosure') 
+    
+class Animal(db.Model): 
+    __tablename__ = 'animals' 
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String) 
+    species = db.Column(db.String) 
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id')) 
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
